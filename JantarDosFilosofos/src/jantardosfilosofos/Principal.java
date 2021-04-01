e4741bd580488eca0c6e166cfa224ad99a7305cf
jeyzandra <jeyzandrasilva@gmail.com>
2021-03-31 21:38:59 -0300

    Create Principal.java

---
+++ JantarDosFilosofos/src/jantardosfilosofos/Principal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jantardosfilosofos;
+
+/**
+ *
+ * @author jeyza
+ */
+public class Principal {
+    
+    public static void main(String[] args) {
+    
+         Filosofos f = new Filosofos();
+         f.test();
+    }     
+}
